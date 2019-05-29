892bd9d5da67b72513b55dc5d073c9b6abcc28ba
Jonathan Cardinal <cardidev@outlook.com>
2019-05-29 07:37:08 -0400

    Mongoose: login sauvegarde maintenant vers db-locale git add .Attention aucune encryption pour le momentgit add . // Fix sur les pages login et subscribe sur les variable avec body-parser

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
 //INIT//////////////////////////////////////////////////////////////////////////
+
 const
 	express = require("express"),
 	app = express(),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose');
+
 //Config////////////////////////////////////////////////////////////////////////
+
+//EXPRESS/////////////////////////////////////////////
 app.use(express.static("public"));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//MONGOOSE////////////////////////////////////////////
+mongoose.connect('mongodb://localhost/FriteSauce', {useNewUrlParser: true});
+const db = mongoose.connection;
+
+////Error Checking on connection
+db.on('error', console.error.bind(console, 'connection error:'));
+
+////Logging connection on local DBS
+db.once('open', () => {
+	console.log("Connectez a la base de donne local");
+});
+
+////DB Connection Logging
+//console.log(db)
+
+////Mongoose Schema Setup (userSchema)
+const userSchema = new mongoose.Schema({
+	email: String,
+	username: String,
+	password: String
+});
+
+////Mongoose Schema Setup (ProductSchema)
+const productSchema = new mongoose.Schema({
+	title: String,
+	description: String,
+	img: String,
+	price: Number
+});
+
+////Mongoose Compile Model pour userSchema
+const User = mongoose.model('User', userSchema);
+////Mongoose Compile Model pour productSchema
+const Product =  mongoose.model('Product', productSchema);
+
+
 //Route////////////////////////////////////////////////////////////////////////
+
 //ROOT///////////////////////////////////////////////
 app.get("/", (req, res) => {
 	res.render("pages/index.ejs");
@@ -16,32 +59,54 @@ app.get("/", (req, res) => {
 //LOGIN//////////////////////////////////////////////
 app.get("/login", (req,res) => {
 	res.render('pages/login.ejs');
-})
+});
 //New User
 app.get("/newuser", (req,res) => {
-	res.render('pages/subscribe.ejs')
+	res.render('pages/subscribe.ejs');
+
+});
+app.post('/usercreate', (req,res) => {
+	//Recois data de l'usager qui s'inscris
+	let username = req.body.username;
+	let password = req.body.password;
+	
+	//Joint le schema a la let user
+	let user = new User({username: username, password: password});
+	
+	//Save user a la db
+	user.save( (err, user) => {
+		if(!err){
+			console.log(user);
+		}
+		else{
+			console.log(err);
+		}
+	})
+	
+	//redirect vers login
+	res.redirect("/login")
 })
-//POST Route pour 
+//POST Route pour utilisateur qui s'enregis
 app.post("/login", (req,res) => {
 	//Post to db
-		//log res
-		console.log(res)
+
 	//Error check
 
 	//redirect
 	res.redirect('/user');
-})
+});
 
 //Logged In///////////////////////////////////////////
+
 //USERPANEL
 app.get('/user', (req,res) => {
-	res.render('pages/user.ejs')
-})
+	res.render('pages/user.ejs');
+});
 
 //ORDER///////////////////////////////////////////////
 app.get("/order", (req,res) => {
-	res.render('pages/order.ejs')
-})
+	res.render('pages/order.ejs');
+});
 
 //Listener///////////////////////////////////////////////////////////////////////
-app.listen((process.env.IP, process.env.PORT || 3000), err =>{console.log(err)});
+app.listen((process.env.IP, process.env.PORT || 3000), err =>{console.log(err);});
